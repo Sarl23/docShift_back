0741f17a729320f2230d7f39dba4ec10b7e5b056
Sarl23 <sergioandresrojasleon@gmail.com>
2024-10-16 23:11:12 -0500

    chore: :art: New controller in shifts
    Create new endpoint to search shift by user id

--- src/controllers/shiftController.js
+++ src/controllers/shiftController.js
@@ -16,7 +16,7 @@ const getAllShift = async (req, res) => {
                 ...shift
             }
         }));
-        res.json(shiftData);
+        res.status(201).json(shiftData);
     } catch (error) {
         console.error('Error getting shift', error);
         res.status(500).send('Internal Server Error getting shift');
@@ -69,12 +69,37 @@ const getShiftById = async (req, res) => {
                 }
             });
         }
-        res.json({ id: shiftDoc.id, ...shiftDoc.data() })
+        res.status(201).json({ id: shiftDoc.id, ...shiftDoc.data() })
     } catch (error) {
         console.log('Error fetching shiftTye');
         res.status(500).send('Internal server error');
     }
 };
+
+
+const getShiftByUserId = async (req, res) => {
+    try {
+        const { companyId, userId } = req.params;
+        const db = await getDb();
+        if (!db) {
+            throw new Error('FireStore has no been initialized');
+        }
+        const querySnapshot = await db.collection('companies').doc(companyId).collection('shift').get();
+        let arrayShiftDataByUser = []
+        querySnapshot.docs.map(doc => {
+            const [shortShiftId, user_Id, dateShift] = doc.id?.split('_');
+            console.log({ shortShiftId, user_Id, dateShift });
+            if (user_Id === userId) {
+                arrayShiftDataByUser.push({ id: doc?.id, ...doc.data() });
+            }
+        });
+        res.status(201).json(arrayShiftDataByUser);
+    } catch (error) {
+        console.error('Error fetching shift by user id', error);
+        res.status(500).send('Internal server error');
+    }
+
+}
 
 const updateShiftById = async (req, res) => {
     try {
@@ -112,5 +137,6 @@ module.exports = {
     postCreateShift,
     getShiftById,
     updateShiftById,
-    deleteShift
+    deleteShift,
+    getShiftByUserId,
 }
--- src/routes/shiftRoutes.js
+++ src/routes/shiftRoutes.js
@@ -4,13 +4,15 @@ const {
     postCreateShift,
     getShiftById,
     updateShiftById,
-    deleteShift
+    deleteShift,
+    getShiftByUserId
 } = require('../controllers/shiftController.js');
 
 const router = Router();
 
 router.get('/companies/:companyId/shift', getAllShift);
 router.get('/companies/:companyId/shift/:shiftId', getShiftById);
+router.get('/companies/:companyId/shift_by_user/:userId', getShiftByUserId);
 router.post('/companies/:companyId/shift', postCreateShift);
 router.put('/companies/:companyId/shift/:shiftId', updateShiftById);
 router.delete('/companies/:companyId/shift/:shiftId', deleteShift);
