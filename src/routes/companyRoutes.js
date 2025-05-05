cce2c7390a067e59c1f681ef83228fe32ac1ff59
Sarl23 <sergioandresrojasleon@gmail.com>
2025-05-04 21:55:38 -0500

    chore: rename routes in company

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -11,8 +11,8 @@ const {
 const router = Router();
 
 router.get('/companies', getAllCompanies);
-router.get('/company_id/:id', getCompanyById);
-router.get('/company_code/:code', getCompanyByCode);
+router.get('/companyId/:id', getCompanyById);
+router.get('/companyCode/:code', getCompanyByCode);
 router.delete('/company/:id', deleteCompany); 
 router.post('/company', postCreateCompany);
 router.put('/company/:id', putCompanyById);
